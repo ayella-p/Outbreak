e688b728c9f0322413c086818d8fae5858ee5206
Ayella Pausal <pausalayella2@gmail.com>
2025-11-17 22:11:34 +0800

    Wrap backstory line for better code readability

--- game/characters/Anya.java
+++ game/characters/Anya.java
@@ -4,7 +4,11 @@ import game.core.Character;
 import game.core.Skill;
 public class Anya extends Character {
     public Anya() {
-        super("Anya", 100, 90, "Resolve", "An ex-special forces sniper, Anya is a master of stealth and long-range combat. She was on a classified mission when the vaccine was distributed, and her unique circumstances shielded her from its effects. Driven by the loss of her entire unit, she now fights to prevent anyone else from experiencing the same horror.");
+        super("Anya", 100, 90, "Resolve", "An ex-special forces " +
+                "sniper, Anya is a master of stealth and long-range combat. She was on a classified " +
+                "mission when the vaccine was distributed, and her unique circumstances shielded her " +
+                "from its effects. Driven by the loss of her entire unit, she now fights to prevent " +
+                "anyone else from experiencing the same horror.");
         skills.add(new Skill("Headshot", 20, 15));
         skills.add(new Skill("Suppressive Fire", 10, 5));
         skills.add(new Skill("Camouflage", 3, 2));
--- game/characters/Zor.java
+++ game/characters/Zor.java
@@ -4,7 +4,11 @@ import game.core.Character;
 import game.core.Skill;
 public class Zor extends Character {
     public Zor() {
-        super("Zor", 100, 70, "Energy", "A former deep-cover operative, Zor's training focused on infiltration and espionage. He was one of the few who, due to a rare genetic anomaly, was naturally immune to the vaccine's effects. He uses his enhanced skills to fight for a government that abandoned its people, his only motivation a desire to see justice served.");
+        super("Zor", 100, 70, "Energy", "A former deep-cover" +
+                " operative, Zor's training focused on infiltration and espionage. He was one of the" +
+                " few who, due to a rare genetic anomaly, was naturally immune to the vaccine's effects." +
+                " He uses his enhanced skills to fight for a government that abandoned its people," +
+                " his only motivation a desire to see justice served.");
         skills.add(new Skill("Sword Slash", 20, 10));
         skills.add(new Skill("High-Jump", 5, 3));
         skills.add(new Skill("Stealth", 10, 3));
